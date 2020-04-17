76812c4402ce60abd996c9d4b8d396f54459eb6d
김기은 <w.snowhack@gmail.com>
2020-04-17 13:59:24 +0900

    feat: Image get & Return Dishes

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -65,9 +65,30 @@ def image_upload(request):
   img = request.FILES.get('file').read()
   # 배열 변환
   img = tf.image.decode_jpeg(img)
-  plt.imshow(img)
-  plt.show()
+  # plt.imshow(img)
+  # plt.show()
   # embed()
-  pass
+  # 사진에서 인식한 재료
+  get_materials = ['베이컨', '양배추', '소고기', '대파', '쌀', '고구마 큰거', '올리브유', '소면', '물', '포도']
+  # 사용자가 선택할 조미료
+  get_cond = ['설탕', '소금']
+  all_materials = set(get_materials + get_cond)
+
+  all_dishes = RecipeBasicInfo.objects.all()
+  dish_list = []
+  for dish in all_dishes:
+    recipe_materials = RecipeMaterialInfo.objects.filter(material_code=dish.basic_code)
+    cnt = 0
+    for r_m in recipe_materials:
+      for a_m in all_materials:
+        if r_m.material_name == a_m:
+          cnt += 1
+
+    if len(recipe_materials) == cnt:
+      dish_list.append(dish.basic_code)
+  # embed()
+  dish_list = RecipeBasicInfo.objects.filter(basic_code__in=dish_list)
+  serializer = RecipeBasicInfoSerializer(dish_list, many=True)
+  return Response(serializer.data)
 
   
