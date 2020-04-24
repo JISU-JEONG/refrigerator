e4289f65d76cfb446a6f5e75398a19cc559e28bb
yg1110 <younggil9488@gmail.com>
2020-04-24 15:47:32 +0900

    재료선택 수정

--- frontend/src/view/VuexExample.vue
+++ frontend/src/view/VuexExample.vue
@@ -1,36 +1,11 @@
 <template>
-  <div>
-    <div class="vuex-example-container">
-      <v-btn @click="VuexTestButtonClick()">Vuex Test</v-btn>
-      {{ receivedVuexValue }}
-    </div>
-  </div>
+  <div></div>
 </template>
 
 <script>
 export default {
   name: "VuexExample",
   data: () => ({}),
-  computed: {
-    receivedVuexValue() {
-      return this.$store.state.name;
-    }
-  },
-  methods: {
-    VuexTestButtonClick() {
-      const payload = {
-        name: "abc"
-      };
-      this.$store.dispatch("VuexTest", payload);
-    }
-  }
+  methods: {}
 };
 </script>
-
-<style scoped>
-.vuex-example-container {
-  width: 100%;
-  height: 100vh;
-  padding-top: 80px;
-}
-</style>
