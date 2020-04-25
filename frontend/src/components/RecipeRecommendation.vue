452891acb676fff4c9d889a9cca3f39842a8552e
yg1110 <younggil9488@gmail.com>
2020-04-25 14:14:30 +0900

    페이지 전환 수정중

--- frontend/src/components/RecipeRecommendation.vue
+++ frontend/src/components/RecipeRecommendation.vue
@@ -1,27 +1,34 @@
 <template>
   <div class="container">
-    <RecipeRecommendationMaterial :materials="materials" />
-    <RecipeRecommendationIcon @icondata="parents" />
-    <br />
-    <v-btn class="btn" color="light-green" @click="click"
-      >레시피 검색하기</v-btn
-    >
+    <RecipeList v-if="showListPage" :recipeInfoArr="recipeInfoArr" />
+    <div v-else>
+      <RecipeRecommendationMaterial :materials="materials" />
+      <RecipeRecommendationIcon @icondata="parents" />
+      <br />
+      <v-btn class="btn" color="light-green" @click="searchRecipe"
+        >레시피 검색하기</v-btn
+      >
+    </div>
   </div>
 </template>
 
 <script>
 import RecipeRecommendationMaterial from "./RecipeRecommendationMaterial";
 import RecipeRecommendationIcon from "./RecipeRecommendationIcon";
+import RecipeList from "../view/RecipeList";
 import http from "../services/http-common.js";
 
 export default {
   name: "RecipeRecommendation",
   components: {
     RecipeRecommendationMaterial,
-    RecipeRecommendationIcon
+    RecipeRecommendationIcon,
+    RecipeList
   },
   data: () => ({
-    condiments: []
+    condiments: [],
+    recipeInfoArr: [],
+    showListPage: false
   }),
   props: {
     materials: {
@@ -33,14 +40,26 @@ export default {
       this.components = name;
       console.log(name);
     },
-    click() {
+    searchRecipe() {
       const data = {
         materials: this.materials,
         condiments: this.components
         // condiments: ["설탕", "소금", "후추", "참기름"]
       };
       http.post("/recipes/get_dishes/", data).then(res => {
+        this.recipeInfoArr = res.data;
+        this.showListPage = !this.showListPage;
         console.log(res);
+      });
+    },
+    onMultiLabel() {
+      this.loading = !this.loading;
+      http.post("/recipes/image_upload/", this.uploadedImage).then(res => {
+        this.loading = !this.loading;
+        this.materials = res.data.materials;
+        this.showMaterialPage = !this.showMaterialPage;
+        this.removeTransparentClass();
+        console.log(res.data.materials);
       });
     }
   }
