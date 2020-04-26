f9ae55e50cebdcd2bbac5b1638add9feb0f29e73
김기은 <w.snowhack@gmail.com>
2020-04-26 20:32:56 +0900

    feat: mode & mlb global variable

--- backend/detect.py
+++ backend/detect.py
@@ -12,15 +12,16 @@ import cv2
 import os
 from IPython import embed
 # construct the argument parse and parse the arguments
-def detect(image):
+def detect(image, model, mlb):
+    embed()
     image = cv2.resize(image, (299, 299))
     image = image.astype("float") / 255.0
     image = img_to_array(image)
     image = np.expand_dims(image, axis=0)
-
-    model = load_model('fashion.model')
-    mlb = pickle.loads(open('mlb.pickle', "rb").read())
+    embed()
+    # model = load_model('fashion.model')
+    # mlb = pickle.loads(open('mlb.pickle', "rb").read())
 
     proba = model.predict(image)[0]
-
+    embed()
     return zip(mlb.classes_, proba)
